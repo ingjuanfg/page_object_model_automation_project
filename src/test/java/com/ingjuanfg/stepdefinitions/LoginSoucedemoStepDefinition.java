37f6bbb7dda8190c0dc8dd5657738790762f5106
Juan Camilo Londoño <londono652@gmail.com>
2022-06-13 20:42:44 -0500

    se adicionan cambios de version en library

--- src/test/java/com/ingjuanfg/stepdefinitions/LoginSoucedemoStepDefinition.java
+++ src/test/java/com/ingjuanfg/stepdefinitions/LoginSoucedemoStepDefinition.java
@@ -4,33 +4,26 @@ import com.ingjuanfg.pages.LoginPage;
 import cucumber.api.java.es.Cuando;
 import cucumber.api.java.es.Dado;
 import cucumber.api.java.es.Entonces;
-import net.thucydides.core.annotations.Managed;
 import org.fluentlenium.core.annotation.Page;
-import org.openqa.selenium.WebDriver;
 
 public class LoginSoucedemoStepDefinition {
 
     @Page
     private LoginPage loginPage;
 
-    @Managed
-    WebDriver driver;
-
     @Dado("que el usuario se encuentra en la pagina")
     public void queElUsuarioSeEncuentraEnLaPagina() {
-         loginPage.open();
+        loginPage.open();
     }
 
     @Cuando("el usuario ingrese sus credenciales")
     public void elUsuarioIngreseSusCredenciales() {
-       loginPage.escribirUsuario("standard_user");
-       loginPage.escribirPassword("secret_sauce");
-       loginPage.clickLogin();
+        loginPage.escribirUsuario("standard_user");
+        loginPage.escribirPassword("secret_sauce");
+        loginPage.clickLogin();
     }
 
-    @Entonces("el usuario deberia entrar el ecomerce")
-    public void elUsuarioDeberiaEntrarElEcomerce() {
-
+    @Entonces("el usuario deberia ingresar al ecommerce")
+    public void elUsuarioDeberiaIngresarAlEcommerce() {
     }
-
 }
