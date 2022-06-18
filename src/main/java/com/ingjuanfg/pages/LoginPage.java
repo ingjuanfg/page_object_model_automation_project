b84baea7c71305e4d618495097f1703e316fa9ec
juferz <juan.isfg@gmail.com>
2022-06-18 07:09:08 -0500

    screenplay

--- src/main/java/com/ingjuanfg/pages/LoginPage.java
+++ src/main/java/com/ingjuanfg/pages/LoginPage.java
@@ -1,6 +1,5 @@
 package com.ingjuanfg.pages;
 
-
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
@@ -9,13 +8,10 @@ import org.openqa.selenium.WebElement;
 
 @DefaultUrl("https://www.saucedemo.com/")
 public class LoginPage extends PageObject {
-
     @FindBy(id="user-name")
     WebElementFacade inpUsuario;
-
     @FindBy(id="password")
     WebElement inpPass;
-
     @FindBy(id="login-button")
     WebElement btnLogin;
 
@@ -25,10 +21,8 @@ public class LoginPage extends PageObject {
 
     public void typePass(String User){
         inpPass.sendKeys(User);
-
     }
     public void clickToLogin(){
         btnLogin.click();
     }
-
 }
